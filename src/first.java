0bc721679125f501e4bc79b3cca29b814b9e83b9
sujatha-94 <sujathakarri701@gmail.com>
2024-07-10 12:07:39 +0530

    first commit

---
+++ src/first.java
@@ -0,0 +1,14 @@
+public class first {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        int count=10;
+        System.out.println("count value is" +count);
+        float f=3.245f;
+        System.out.println(f);
+        String s="sujatha";
+        System.out.println(s);
+        double a=3.235;
+        System.out.println(a);
+
+    }
+}
