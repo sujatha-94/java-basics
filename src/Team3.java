0c7086a10cba6937189b38ee57a881b52c70d9a0
sujatha-94 <sujathakarri701@gmail.com>
2024-07-11 12:31:25 +0530

    hiii

---
+++ src/Team3.java
@@ -0,0 +1,11 @@
+public class Team3 {
+    public void sujatha() {
+        System.out.println("sujatha");
+    }
+    public void swarupa() {
+        System.out.println("swarupa");
+    }
+    public void kavitha() {
+        System.out.println("kavitha");
+    }
+}
